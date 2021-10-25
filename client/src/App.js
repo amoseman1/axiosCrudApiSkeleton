d920edaeb8851e358896ccaec6a9f3719919bbb5
Amy Moseman <amoseman18@gmail.com>
2021-10-25 12:37:00 -0600

    created react app, added bootstrap styling link

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Axios Demo</h2>
+    </div>
+  );
+}
+
+export default App;
